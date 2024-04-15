ef1cba53dabd4933a47c3606cfff05b4c8d544c7
DonGuillotine <infect3dlab@gmail.com>
2024-04-15 09:33:42 +0100

    Switch Statements in Ruby

---
+++ conditionals_switch.rb
@@ -0,0 +1,12 @@
+test_score = "A"
+
+case test_score
+when "A"
+    puts "You excelled"
+when "B"
+    puts "You passed"
+when "F"
+    puts "You Failed!"
+else
+    puts "You didnt write the exam"
+end
