58750bfed36a2096ba3837dc7a153709018e534f
DonGuillotine <infect3dlab@gmail.com>
2024-04-15 09:07:22 +0100

    N dimentional arrays in Ruby

---
+++ n_dimentional_arrays.rb
@@ -0,0 +1,18 @@
+almighty = [[2,3], [5,3]]
+
+puts almighty[0][0]
+puts almighty[0][1]
+
+puts "\n\n"
+
+puts almighty[1][0]
+puts almighty[1][1]
+
+puts "\n\n"
+
+puts almighty[0][0] = 144000
+puts almighty[1][1] = -900
+
+puts "\n\n"
+
+puts almighty
