43174de9b2977cab4066e8489ddc18f5470b95cf
DonGuillotine <infect3dlab@gmail.com>
2024-04-15 08:47:04 +0100

    Numbers and Number Methods in Ruby

---
+++ numbers.rb
@@ -0,0 +1,17 @@
+puts 2 * 3
+puts 2**3
+puts 4 % 2
+puts 10 / 3.0 # The data type of the divider matters, divide with float? You get float. Divide with integer? You get integer
+puts (2 + 3) * 4 # Ruby follows order of operation ie BODMAS
+
+
+num = 10
+num += 90
+puts num
+
+numero = -70.5
+puts numero.abs() # Returns absolute number
+puts numero.round()
+
+puts Math.sqrt(144)
+puts Math.log(0)
