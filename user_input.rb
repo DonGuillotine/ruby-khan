ca82acede4ad7622afc4419f21c7c4bd9a93fef8
DonGuillotine <infect3dlab@gmail.com>
2024-04-15 08:53:57 +0100

    Collecting User input in Ruby

---
+++ user_input.rb
@@ -0,0 +1,10 @@
+puts "Enter your Name"
+name = gets.chomp
+puts name = "Your name is #{name}"
+
+puts "Enter a number"
+first_num = gets.chomp
+puts "Enter second number"
+second_num = gets.chomp
+total = first_num.to_f + second_num.to_f
+puts "The sum is #{total}"
