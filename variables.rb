84d52df516e235f29ff71cd99cd26086f6b01992
DonGuillotine <infect3dlab@gmail.com>
2024-04-15 08:21:32 +0100

    Variables in Ruby

---
+++ variables.rb
@@ -0,0 +1,14 @@
+# This is a single line comment. There are strings, numbers, decimals and boolean data types in Ruby
+
+=begin
+This is a multi line comment 
+There are strings, numbers, decimals and boolean data types in Ruby
+=end
+
+name = "Donald"
+age = 24
+salary = 12.5
+need_starlink = true
+
+puts name = "Nwokoro"
+puts "Your name is #{name}"
